24c4b37447dcc815e3c9620ed8282a64692d3dcd
NotAnsar <karroauch.ansar@gmail.com>
2023-08-23 02:06:54 +0100

    fixed db

--- connect.js
+++ connect.js
@@ -1,16 +1,49 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+// 	host: process.env.DB_host,
+// 	port: process.env.DB_port,
+// 	user: process.env.DB_user,
+// 	password: process.env.DB_password,
+// 	database: process.env.DB_database,
+// });
+
+// db.connect(function (err) {
+// 	if (err) throw err;
+// 	console.log('Connected!');
+// });
+
+const db_config = {
 	host: process.env.DB_host,
 	port: process.env.DB_port,
 	user: process.env.DB_user,
 	password: process.env.DB_password,
 	database: process.env.DB_database,
-});
+};
 
-db.connect(function (err) {
-	if (err) throw err;
-	console.log('Connected!');
-});
+let db;
+
+function handleDisconnect() {
+	db = mysql.createConnection(db_config);
+
+	db.connect((err) => {
+		if (err) {
+			console.log('Error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000);
+		}
+		console.log('Connected!');
+	});
+
+	db.on('error', (err) => {
+		console.log('DB error:', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+handleDisconnect();
 
 module.exports = db;
